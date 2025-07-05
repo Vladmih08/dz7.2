c6283232263cdb90aa69088b4219f4ab9167c52d
vladmшр08 <vladmih2008@gmail.com>
2025-07-05 14:56:58 +0300

    Перша версія коду, дуже подобається assert: досить цікаво підвлаштовувати свій код під задані умови.

---
+++ dz7.2.py
@@ -0,0 +1,13 @@
+import string
+def correct_sentence(string1):
+    string1 = string1[0].upper() + string1[1:]
+    if string1[-1] == ".":
+        return string1
+    else:
+        return (string1 + '.')
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
